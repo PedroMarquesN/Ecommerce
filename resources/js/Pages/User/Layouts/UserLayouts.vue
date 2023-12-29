abf26f4e33567c400adc3eea6c6f3d7ed9de2d4a
Pedro Marques <pedroleagueartmarques@gmail.com>
2023-12-28 21:57:35 -0300

    Componentizando Home

---
+++ resources/js/Pages/User/Layouts/UserLayouts.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import Hero from "./Hero.vue";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+});
+
+</script>
+<template>
+    <!--Header-->
+<Header></Header>
+    <!--End Header-->
+    <!--Hero Propaganda-->
+<Hero></Hero>
+    <!-- END Hero Propaganda-->
+    <!--Main content-->
+    <slot />
+    <!-- END Main content-->
+    <!--footer-->
+<Footer></Footer>
+    <!--end - footer-->
+</template>
