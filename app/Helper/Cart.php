b8a7b156e9d559af8406fd9fe64ba0aa6c044511
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-08 14:35:46 -0300

    Corrigido bug cart e cookie

--- app/Helper/Cart.php
+++ app/Helper/Cart.php
@@ -2,6 +2,8 @@
 
 namespace App\Helper;
 use App\Models\CartItem;
+use App\Models\Product;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cookie;
 
 class Cart
@@ -10,13 +12,19 @@ class Cart
     {
         if($user = auth()->user()){
             return CartItem::whereUserId($user->id)->sum('quantity');
+        }else{
+            return array_reduce(self::getCookieCartItems(), fn($carry, $item) => $carry + $item['quantity'],0);
         }
+
+
     }
 
     public static function getCartItems()
     {
         if($user = auth()->user()){
             return CartItem::whereUserId($user->id)->get()->map(fn (CartItem $item) => ['product_id' => $item->product_id, 'quantity' => $item->quantity]);
+        }else{
+            return self::getCookieCartItems();
         }
     }
 
@@ -25,10 +33,11 @@ class Cart
         return json_decode(request()->cookie('cart_items', '[]'), true);
     }
 
-    public static function setCookieCartItems()
+    public static function setCookieCartItems(array $cartItems)
     {
-        Cookie::queue('cart_items', fn(int $carry, array $item)=> $carry + $item['quantity'],0);
+        Cookie::queue('cart_items', json_encode($cartItems), 60*24*30);
     }
+
 
     public static function saveCookCartItems()
     {
@@ -75,6 +84,16 @@ class Cart
         if(!empty($newCartItems)){
             CartItem::insert($newCartItems);
         }
+    }
+
+    public static function getProductsAndCartItems() 
+    {
+        $cartItems = self::getCartItems();
+        $ids = Arr::pluck($cartItems, 'product_id');
+        $products = Product::whereIn('id', $ids)->with('product_images')->get();
+        $cartItems = Arr::keyBy($cartItems, 'product_id');
+        return[$products, $cartItems];
+
     }
 
 }
