54ef2f0cd7bb75cde890582193520db205da7972
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-11 16:14:18 -0300

    adicionado outros cards in admin dashboard

--- resources/js/Pages/Admin/Components/Dashboard.vue
+++ resources/js/Pages/Admin/Components/Dashboard.vue
@@ -12,87 +12,12 @@ defineProps({
     totalPrice: Array,
     orders: Array
 });
-//GRAFICS
-  // ApexCharts options and config
-  window.addEventListener("load", function() {
-    let options = {
-      chart: {
-        height: "100%",
-        maxWidth: "100%",
-        type: "area",
-        fontFamily: "Inter, sans-serif",
-        dropShadow: {
-          enabled: false,
-        },
-        toolbar: {
-          show: false,
-        },
-      },
-      tooltip: {
-        enabled: true,
-        x: {
-          show: false,
-        },
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          opacityFrom: 0.55,
-          opacityTo: 0,
-          shade: "#1C64F2",
-          gradientToColors: ["#1C64F2"],
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        width: 6,
-      },
-      grid: {
-        show: false,
-        strokeDashArray: 4,
-        padding: {
-          left: 2,
-          right: 2,
-          top: 0
-        },
-      },
-      series: [
-        {
-          name: "New users",
-          data: [6500, 6418, 6456, 6526, 6356, 6456],
-          color: "#1A56DB",
-        },
-      ],
-      xaxis: {
-        categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
-        labels: {
-          show: false,
-        },
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-      },
-      yaxis: {
-        show: false,
-      },
-    }
-
-    if (document.getElementById("area-chart") && typeof ApexCharts !== 'undefined') {
-      const chart = new ApexCharts(document.getElementById("area-chart"), options);
-      chart.render();
-    }
-  });
-// END GRAFICS
 
 
 
 onMounted(() => {
     initFlowbite();
+
 });
 </script>
 
@@ -117,13 +42,15 @@ onMounted(() => {
 
                         </div>
                         <div class="pt-1 text-right">
-                            <p class="text-sm capitalize font-bold">Produtos</p>
-                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-2xl">{{ products }}</h4>
+                            <p class="text-4xl capitalize font-bold">Produtos</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">{{ products }}</h4>
                         </div>
                     </div>
                     <hr class="opacity-50" />
                     <div class="p-4">
-                        <p class="font-light"><span class="text-sm font-bold text-green-600">+22% </span>vs last month</p>
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">VER
+                            INFORMAÇÕES</button>
                     </div>
                 </div>
             </div>
@@ -142,13 +69,16 @@ onMounted(() => {
                             </svg>
                         </div>
                         <div class="pt-1 text-right">
-                            <p class="text-sm font-light capitalize">Usuários</p>
-                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-2xl">{{ users }}</h4>
+                            <p class="text-4xl font-bold capitalize">Usuários</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">{{ users }}</h4>
                         </div>
                     </div>
                     <hr class="opacity-50" />
                     <div class="p-4">
-                        <p class="font-light"><span class="text-sm font-bold text-green-600">+3% </span>vs last month</p>
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">VER
+                            INFORMAÇÕES</button>
+
                     </div>
                 </div>
             </div>
@@ -167,13 +97,16 @@ onMounted(() => {
                             </svg>
                         </div>
                         <div class="pt-1 text-right">
-                            <p class="text-sm font-light capitalize">Vendas</p>
-                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-2xl">${{ totalPrice }}</h4>
+                            <p class="text-4xl font-bold capitalize">Vendas</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">${{ totalPrice }}</h4>
                         </div>
                     </div>
                     <hr class="opacity-50" />
                     <div class="p-4">
-                        <p class="font-light"><span class="text-sm font-bold text-red-600">-3% </span>vs last month</p>
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">VER
+                            INFORMAÇÕES</button>
+
                     </div>
                 </div>
             </div>
@@ -193,81 +126,138 @@ onMounted(() => {
 
                         </div>
                         <div class="pt-1 text-right">
-                            <p class="text-sm font-light capitalize">Pedidos</p>
-                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-2xl">{{ orders }}</h4>
+                            <p class="text-4xl font-bold capitalize">Pedidos</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">{{ orders }}</h4>
                         </div>
                     </div>
                     <hr class="opacity-50" />
                     <div class="p-4">
-                        <p class="font-light"><span class="text-sm font-bold text-red-600">-3% </span>vs last month</p>
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            VER INFORMAÇÕES</button>
+
                     </div>
+                </div>
+            </div>
+            <!-- Card 5: Produtos -->
+            <div class="flex w-full mt-12">
+                <div
+                    class="flex w-full max-w-full flex-col break-words rounded-lg border border-gray-100 bg-white text-gray-600 shadow-lg">
+                    <div class="p-3">
+                        <div
+                            class="absolute -mt-10 h-16 w-16 rounded-xl bg-gradient-to-tr from-gray-700 to-gray-400 text-center text-white shadow-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="mt-4 h-7 w-16" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+
+                        </div>
+                        <div class="pt-1 text-right">
+                            <p class="text-4xl capitalize font-bold">Visualizações</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">1.400</h4>
+                        </div>
+                    </div>
+                    <hr class="opacity-50" />
+                    <div class="p-4">
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">VER
+                            INFORMAÇÕES</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 6: Usuários -->
+            <div class="flex w-full mt-12">
+                <div
+                    class="flex w-full max-w-full flex-col break-words rounded-lg border border-gray-100 bg-white text-gray-600 shadow-lg">
+                    <div class="p-3">
+                        <div
+                            class="absolute -mt-10 h-16 w-16 rounded-xl bg-gradient-to-tr from-blue-700 to-blue-500 text-center text-white shadow-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="mt-4 h-7 w-16">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+                            </svg>
+
+                        </div>
+                        <div class="pt-1 text-right">
+                            <p class="text-4xl font-bold capitalize">Pagamentos</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">14</h4>
+                        </div>
+                    </div>
+                    <hr class="opacity-50" />
+                    <div class="p-4">
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">VER
+                            INFORMAÇÕES</button>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 7: Vendas -->
+            <div class="flex w-full mt-12">
+                <div
+                    class="flex w-full max-w-full flex-col break-words rounded-lg border border-gray-100 bg-white text-gray-600 shadow-lg">
+                    <div class="p-3">
+                        <div
+                            class="absolute -mt-10 h-16 w-16 rounded-xl bg-gradient-to-tr from-emerald-700 to-emerald-500 text-center text-white shadow-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="mt-4 h-7 w-16">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                            </svg>
+
+                        </div>
+                        <div class="pt-1 text-right">
+                            <p class="text-4xl font-bold capitalize">Marcas</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">5</h4>
+                        </div>
+                    </div>
+                    <hr class="opacity-50" />
+                    <div class="p-4">
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">VER
+                            INFORMAÇÕES</button>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 8: Outro Card -->
+            <div class="flex w-full mt-12">
+                <div
+                    class="flex w-full max-w-full flex-col break-words rounded-lg border border-gray-100 bg-white text-gray-600 shadow-lg">
+                    <div class="p-3">
+                        <div
+                            class="absolute -mt-10 h-16 w-16 rounded-xl bg-gradient-to-tr from-emerald-700 to-emerald-500 text-center text-white shadow-lg">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="mt-4 h-7 w-16">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6" />
+                            </svg>
+
+
+                        </div>
+                        <div class="pt-1 text-right">
+                            <p class="text-4xl font-bold capitalize">Categorias</p>
+                            <h4 class="text-2xl font-semibold tracking-tighter xl:text-4xl mt-2 shadow-md">{{ orders }}</h4>
+
+
+                        </div>
+                    </div>
+                    <hr class="opacity-50" />
+                    <div class="p-4">
+                        <button type="button"
+                            class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            VER INFORMAÇÕES</button>
+
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <!--START GRAFICS-->
-    <div>
-        <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-  <div class="flex justify-between">
-    <div>
-      <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">32.4k</h5>
-      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Users this week</p>
-    </div>
-    <div
-      class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
-      12%
-      <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
-      </svg>
-    </div>
-  </div>
-  <div id="area-chart"></div>
-  <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
-    <div class="flex justify-between items-center pt-5">
-      <!-- Button -->
-      <button
-        id="dropdownDefaultButton"
-        data-dropdown-toggle="lastDaysdropdown"
-        data-dropdown-placement="bottom"
-        class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-        type="button">
-        Last 7 days
-        <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-        </svg>
-      </button>
-      <!-- Dropdown menu -->
-      <div id="lastDaysdropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-          <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-            <li>
-              <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yesterday</a>
-            </li>
-            <li>
-              <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Today</a>
-            </li>
-            <li>
-              <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last 7 days</a>
-            </li>
-            <li>
-              <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last 30 days</a>
-            </li>
-            <li>
-              <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last 90 days</a>
-            </li>
-          </ul>
-      </div>
-      <a
-        href="#"
-        class="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2">
-        Users Report
-        <svg class="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-        </svg>
-      </a>
-    </div>
-  </div>
-</div>
-    </div>
-    <!--END GRAFICS-->
-</AdminLayout>
 
-</template>
+</AdminLayout></template>
